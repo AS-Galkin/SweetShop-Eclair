66e0855a836553dfeac5a0152c4edf5b65c47247
Alexander Galkin <demotivatoris@gmail.com>
2020-12-02 13:01:49 +0300

    Initial Commit

---
+++ SweetShop/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SweetShop
+//
+//  Created by Александр Галкин on 02.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
